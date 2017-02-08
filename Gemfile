b304a222a02cdba5beb959a1fb98fa2f92bed576
Carl de REYNAL <carldereynal@gmail.com>
2017-02-08 15:38:05 +0100

    need to work on style now

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ git_source(:github) do |repo_name|
 end
 
 
+
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
@@ -35,9 +38,18 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# FRONT GEMS
+
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem "better_errors"
+  gem "binding_of_caller"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "01 23 45 67 89",
+    category: "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "01 23 45 67 89",
+    category: "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "01 23 45 67 89",
+    category: "french"
+  }
+]
+
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
+
+Review.destroy_all
+
+reviews_attributes = [
+  {
+    content: "Very good restaurant",
+    rating: 5,
+    restaurant_id: 1
+  },
+
+  {
+    content: "average restaurant",
+    rating: 3,
+    restaurant_id: 2
+  },
+
+  {
+    content: "Very bad restaurant",
+    rating: 0,
+    restaurant_id: 3
+  }
+]
+
+reviews_attributes.each { |params| Review.create!(params) }
